9f0c75fd67a46f1297a9b814a8833d31271ebeb2
Sereen-Kh <sereenmh.90@gmail.com>
2025-12-12 22:25:52 +0300

    Initialize monorepo with client and server packages
    Set up a new monorepo project using Bun, including root configuration, a React client (with Vite), and an Express server. Added initial configuration files, package manifests, TypeScript configs, and README documentation for each package.

---
+++ packages/client/src/App.tsx
@@ -0,0 +1,15 @@
+import { useEffect, useState } from "react";
+
+function App() 
+{
+const [message, setMessage]= useState('');
+
+useEffect(()=>{
+  fetch('/api/hello')
+    .then(response => response.json())
+    .then(data => setMessage(data.message));
+  },[]);
+  return <p>{message}</p>
+}
+
+export default App
